434eb222515a8d69c349227e5a15ea0b9dc24f77
Z H <zjhamm304@gmail.com>
2019-07-23 19:42:03 -0400

    Image module added

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -6,10 +6,20 @@ defmodule Identicon do
 
   @doc """
   Makes an MD5 hash out of a String 'input',
-  then grabs a list of numbers generated from the MD5 hashing.
+  then stores them in Identicon.Image as
+  the List of numbers generated from the MD5 hashing.
+
   """
   def make_hash(input) do
-    :crypto.hash(:md5, input)
-    |> :binary.bin_to_list
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list()
+
+    %Identicon.Image{hex: hex}
   end
+
+  def color_values(hash) do
+  end
+
+  # If num % 2 == 0, color it. else, leave blank
 end
